8d43cc6c32c2c962e58ffe447fb410569f3476fc
Everton Melo <evertonsquaq@gmail.com>
2019-09-27 11:09:21 -0300

    leaner reducers/result

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -4,13 +4,17 @@ import { updateObject } from '../utility'
 const initialState = {
 	results: []
 }
+
+const deleteResult = (state, action) => {
+	let newResults = state.results.filter(idx => idx.id !== action.id)
+	return updateObject(state, {results: newResults}); 
+}
 const reducer = (state = initialState, action) => {
 	switch (action.type) {
 		case actionTypes.STORE_RESULT:
 			return updateObject(state, {results: state.results.concat({id: new Date(), value: action.result})});
 		case actionTypes.DELETE_RESULT:
-			let newResults = state.results.filter(idx => idx.id !== action.id)
-			return updateObject(state, {results: newResults});
+			return deleteResult(state, action);
 		default:
 			return state;
 	}
